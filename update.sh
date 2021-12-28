dee3f2f458d0221577e6954888d4851405cb5f53
x80486 <masked.github@mailgw.com>
2021-12-28 08:15:18 -0500

    refactor: rename variable that holds the (release) tag identifier

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 set -o errexit -o nounset -o pipefail
 
-TAG_NAME="$(curl --fail --show-error --silent --location https://api.github.com/repos/lumeland/lume/releases/latest | jq --raw-output .tag_name)"
+TAG_IDENTIFIER="$(curl --fail --show-error --silent --location https://api.github.com/repos/lumeland/lume/releases/latest | jq --raw-output .tag_name)"
 
-if [[ "${TAG_NAME}" =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
+if [[ "${TAG_IDENTIFIER}" =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
   # printf "Using version %s\n" "${BASH_REMATCH[0]}"
   sed --regexp-extended --in-place "s/ENV LUME_VERSION=.+$/ENV LUME_VERSION=\"${BASH_REMATCH[0]}\"/" ./*/Dockerfile
 else
